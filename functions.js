7e4db72df206921e47e2cd93a1aeb4f001f44b66
justforwebcoding <iromjkeecorp@yandex.ru>
2018-07-17 12:13:07 +0300

    ternary operator added

--- functions.js
+++ functions.js
@@ -3,11 +3,7 @@
 const getTodos = () => {
     const todoJSON = localStorage.getItem('todos')
 
-    if(todoJSON !== null) {
-        return JSON.parse(todoJSON)
-    }  else {
-        return []
-    }   
+    return (todoJSON !== null) ? JSON.parse(todoJSON) : []
 
 }
 
